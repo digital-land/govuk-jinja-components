c6f180a3397c61979b428b0aa5ee8d94c814a19e
Colm Britton <colmjude@gmail.com>
2020-09-25 12:54:50 +0100

    Create list of templates

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+import glob
+import os
+
 from setuptools import setup
+
+components = []
+directories_html = glob.glob("govuk_jinja_components/**/**/*.html", recursive=True)
+directories_jinja = glob.glob("govuk_jinja_components/**/**/*.jinja", recursive=True)
+
+for directory in directories_html:
+    components.append(os.path.relpath(os.path.dirname(directory), "govuk_jinja_components") + "/*.html")
+
+for directory in directories_jinja:
+    components.append(os.path.relpath(os.path.dirname(directory), "govuk_jinja_components") + "/*.jinja")
 
 setup(
     name="govuk-jinja-components",
@@ -7,7 +20,7 @@ setup(
     description="GOVUK Design system components ported from Nunjucks to Jinja",
     license="MIT",
     packages=["govuk_jinja_components"],
-    package_data={'govuk-jinja-components': ['templates/**.*']},
+    package_data={'govuk-jinja-components': components},
     python_requires=">=3.5",
     install_requires=[
         "jinja2",
